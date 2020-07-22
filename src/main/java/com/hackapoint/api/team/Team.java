2b01c2fb255bff2624b098c3dd6c287272c85615
lePitch <pierre.henri.jautard@gmail.com>
2020-07-22 11:27:12 +0200

    feat: team entity

--- src/main/java/com/hackapoint/api/team/Team.java
+++ src/main/java/com/hackapoint/api/team/Team.java
@@ -1,4 +1,66 @@
 package com.hackapoint.api.team;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.hackapoint.api.hackathon.Hackathon;
+import com.hackapoint.api.member.Member;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="team_id")
+    private Long id;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @ManyToOne
+    @JsonBackReference
+    private Hackathon hackathon;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "team")
+    @JsonManagedReference
+    private Set<Member> members;
+
+    private String name;
+
+    public Team() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Hackathon getHackathon() {
+        return hackathon;
+    }
+
+    public void setHackathon(Hackathon hackathon) {
+        this.hackathon = hackathon;
+    }
+
+    public Set<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<Member> members) {
+        this.members = members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
