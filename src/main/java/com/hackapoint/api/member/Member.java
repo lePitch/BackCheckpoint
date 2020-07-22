1c0853b9f9be8e3f3b44177b0e3a9c016a3db42a
lePitch <pierre.henri.jautard@gmail.com>
2020-07-22 11:26:56 +0200

    feat: member entity

--- src/main/java/com/hackapoint/api/member/Member.java
+++ src/main/java/com/hackapoint/api/member/Member.java
@@ -1,4 +1,77 @@
 package com.hackapoint.api.member;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.hackapoint.api.team.Team;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_id")
+    private Long id;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @ManyToOne
+    @JsonBackReference
+    private Team team;
+
+    private String name;
+    private String quote;
+    private String avatar;
+    private String activity;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
 }
