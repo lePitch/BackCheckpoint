9da78543b3b92e176ed3c5e1f8860e01a568d89a
lePitch <pierre.henri.jautard@gmail.com>
2020-07-27 23:47:58 +0200

    update swagger config

--- src/main/java/com/hackapoint/api/config/SwaggerConfig.java
+++ src/main/java/com/hackapoint/api/config/SwaggerConfig.java
@@ -16,8 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.invertabrates.api"))
-                .apis(Predicate.not(RequestHandlerSelectors.basePackage("com.invertabrates.api.counter")))
+                .apis(RequestHandlerSelectors.basePackage("com.hackapoint.api"))
                 .paths(PathSelectors.any())
                 .build();
     }
